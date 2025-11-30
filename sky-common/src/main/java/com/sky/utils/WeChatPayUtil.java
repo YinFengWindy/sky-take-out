56b0694c7d88e966ce7d8b8aafc15109bc833376
YuFeng <3174898512@qq.com>
2025-11-30 18:36:47 +0800

    编辑员工信息功能开发

--- sky-common/src/main/java/com/sky/utils/WeChatPayUtil.java
+++ sky-common/src/main/java/com/sky/utils/WeChatPayUtil.java
@@ -2,7 +2,7 @@ package com.sky.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.sky.properties.WeChatProperties;
+
 import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
 import com.wechat.pay.contrib.apache.httpclient.util.PemUtil;
 import org.apache.commons.lang.RandomStringUtils;
@@ -17,6 +17,7 @@ import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -28,7 +29,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
-
+import com.sky.properties.WeChatProperties;
 /**
  * 微信支付工具类
  */
@@ -41,7 +42,7 @@ public class WeChatPayUtil {
     //申请退款接口地址
     public static final String REFUNDS = "https://api.mch.weixin.qq.com/v3/refund/domestic/refunds";
 
-    @Autowired
+    @Resource
     private WeChatProperties weChatProperties;
 
     /**
